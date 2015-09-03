080feee5fe6ecbbff4c85cef56fd4badb8315ff2
Sinn <gevalentino@gmail.com>
2015-09-02 23:38:54 -0300

    Added number of rules found finder and plotting!

--- Tarea-2/src/plotter.py
+++ Tarea-2/src/plotter.py
@@ -5,42 +5,9 @@ import matplotlib.pyplot as plt
 import subprocess
 from scipy.interpolate import interp1d
 import numpy as np
-
-INPUT = "supermarket.arff"
-RUN = "./run.sh"
-FILTER = "./filter.sh"
-filtered = "supermarket-filtered.arff"
-filter_indexes = map(lambda x: str(x) ,
-                        [
-                            13, # bread and cake
-                            86  # vegetables
-                        ])
-if filter_indexes:
-    cmd = [FILTER, INPUT, filtered,  ",".join(filter_indexes)]
-    subprocess.call(cmd)
-else:
-    filtered = INPUT
-
-c = map(lambda x: str(x/100.0), range(85, 96))
-alg_minsup = {
-    "Apriori": ['0.1', '0.15'],
-    "FP-growth": map(lambda x: str(x/100.0), range(16, 26))
-}
-
-alg_type = {"Apriori":"1", "FP-growth":"2"}
-
-for alg, minsups in alg_minsup.items():
-    for minsup in minsups:
-        for conf in c:
-            filename = '-'.join([alg,minsup,conf])+"-out.txt"
-            cmd = [RUN, filtered, filename, conf, minsup, alg_type[alg]];
-            subprocess.call(cmd)
-exit()
+import re
 
 
-x = range(1,11)
-y  = map(lambda i: i**2, x)
-plotter(x, y)
 
 def plotter(x, y, filename = 'foo.png', interp_points = 30, title = 'Alg wtih Min Sup = minsup', x_title = 'Confidence', y_title = '# Rules'):
     x_interp   = np.linspace(min(x), max(x), interp_points)
@@ -53,8 +20,54 @@ def plotter(x, y, filename = 'foo.png', interp_points = 30, title = 'Alg wtih Mi
 
     ax1.plot(x, y, 'bo', x_interp, y_interp, 'b-')
 
-    plt.savefig(filename, bbox_inches='tight')
+    plt.savefig(filename)
     # plt.show()
 
+
+INPUT = "supermarket.arff"
+RUN = "./run.sh"
+FILTER = "./filter.sh"
+MATCHER = re.compile(r'.*?([\d]+)\. .*')
+filtered = "supermarket-filtered.arff"
+filter_indexes = map(lambda x: str(x) ,
+                        [
+                            13, # bread and cake
+                            86  # vegetables
+                        ])
+if filter_indexes:
+    cmd = [FILTER, INPUT, filtered,  ",".join(filter_indexes)]
+    subprocess.call(cmd)
+else:
+    filtered = INPUT
+c_float = map(lambda x: x/100.0, range(85, 96))
+c = map(lambda x: str(x/100.0), range(85, 96))
+alg_minsup = {
+    "Apriori": ['0.1', '0.15'],
+    "FP-growth": map(lambda x: str(x/100.0), range(16, 26))
+}
+
+alg_type = {"Apriori":"1", "FP-growth":"2"}
+
+for alg, minsups in alg_minsup.items():
+    for minsup in minsups:
+        filename = None
+        ys = []
+        for conf in c:
+            filename = '-'.join([alg,minsup,conf])+"-out.txt"
+            cmd = [RUN, filtered, filename, conf, minsup, alg_type[alg]];
+            subprocess.call(cmd)
+
+            weka_file = open(filename)
+            last = None
+            for line in weka_file:
+                mt = MATCHER.match(line)
+                if mt:
+                    last = mt.groups()[-1]
+            weka_file.close()
+            ys.append(last)
+        graph_name =  alg+"-"+minsup
+        plotter(c_float, ys, filename=graph_name+".png",  title=alg+" MinSup "+minsup)
+        break
+    break
 
 
