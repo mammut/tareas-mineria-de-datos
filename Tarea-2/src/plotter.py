91850abcbc6ab380bd27f3928e8e39df57f95dd3
mammut <irken.cs@gmail.com>
2015-09-09 00:37:15 -0300

    Add time charts

--- Tarea-2/src/plotter.py
+++ Tarea-2/src/plotter.py
@@ -7,6 +7,7 @@ from scipy.interpolate import interp1d
 import numpy as np
 import re
 import os.path
+from time import time
 
 
 
@@ -18,7 +19,7 @@ def plotter(x, y, filename = 'foo.png', interp_points = 30, title = 'Alg wtih Mi
     ax1.set_xlabel(x_title, fontweight = 'bold')
     ax1.set_ylabel(y_title, fontweight = 'bold')
     fig.suptitle(title, fontsize = 14, fontweight = 'bold')
-
+    # ax1.set_ylim([0, 250])
     ax1.plot(x, y, 'bo', x_interp, y_interp, 'b-')
 
     plt.savefig(filename)
@@ -47,17 +48,24 @@ alg_minsup = {
 }
 
 alg_type = {"Apriori":"1", "FP-growth":"2"}
+alg_time = {}
 
 for alg, minsups in alg_minsup.items():
+    alg_time[alg] = {}
     for minsup in minsups:
         filename = None
         ys = []
+        alg_time[alg][minsup] = {'x': [], 'y': []}
         for conf in c:
             filename = 'outs/'+'-'.join([alg,minsup,conf])+"-out.txt"
 
             if not os.path.isfile(filename):
                 cmd = [RUN, filtered, filename, conf, minsup, alg_type[alg]];
+                t1 = time()
                 subprocess.call(cmd)
+                t2 = time()
+                alg_time[alg][minsup]['x'].append(float(conf))
+                alg_time[alg][minsup]['y'].append(t2 - t1)
 
             weka_file = open(filename)
             last = 0
@@ -70,3 +78,14 @@ for alg, minsups in alg_minsup.items():
             ys.append(last)
         graph_name =  alg+"-"+minsup
         plotter(c_float, ys, filename='img/'+graph_name+".png",  title=alg+" MinSup "+minsup)
+        plotter(alg_time[alg][minsup]['x'], alg_time[alg][minsup]['y'], filename='img/'+graph_name+"-exec-conf.png", x_title="Confidence", y_title="Time", title=alg+" Exec time vs Conf "+minsup)
+
+for alg, minsups in alg_minsup.items():
+    xs = []
+    ys = []
+    for minsup in minsups:
+        xs.append(float(minsup))
+        ys.append(alg_time[alg][minsup]['y'][0])
+
+    graph_name =  alg+"-"+minsup
+    plotter(xs, ys, filename='img/'+graph_name+"-exec-minsup.png", x_title="Minsup", y_title="Time", title=alg+" Exec time vs Minsup "+minsup)
